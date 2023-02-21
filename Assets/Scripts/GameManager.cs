c1e46bcc0f71689bcb2c6eda808344a9383e8087
ANHBDHE151175 <ANHBDHE151175@fpt.edu.vn>
2023-02-21 16:27:05 +0700

    resolve score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,7 +21,7 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
 
-        //DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
         SceneManager.sceneLoaded += LoadScene;
 
     }
